0155e5655b292e649ff740413af4f0edd38cda0f
Armina <arminagjoka@yahoo.com>
2023-02-19 10:42:23 +0100

    Create MVC Project

---
+++ Final_Project_Conference_Room_Booking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Project_Conference_Room_Booking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
