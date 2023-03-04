4df4e793996fe981a708caba00ae00b296ca517a
Anxhulja <anxhulja.kurtaj7@gmail.com>
2023-03-04 01:33:30 +0100

    Added Log In Functionality

---
+++ Final_Project_Conference_Room_Booking/Repositories/Implementation/LoginRepository.cs
@@ -0,0 +1,27 @@
+﻿using Final_Project_Conference_Room_Booking.Models;
+using Final_Project_Conference_Room_Booking.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project_Conference_Room_Booking.Repositories.Implementation
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly ConferenceRoomBookingContext _context;
+
+        public LoginRepository(ConferenceRoomBookingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> AuthenticateUser(string email, string passcode)
+        {
+            var succeeded = await _context.Users.FirstOrDefaultAsync(authUser => authUser.Email == email && authUser.Password == passcode);
+            return succeeded;
+        }
+
+        public async Task<IEnumerable<User>> getuser()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
---
+++ Final_Project_Conference_Room_Booking/Repositories/Interfaces/ILoginRepository.cs
@@ -0,0 +1,10 @@
+﻿using Final_Project_Conference_Room_Booking.Models;
+
+namespace Final_Project_Conference_Room_Booking.Repositories.Interfaces
+{
+    public interface ILoginRepository
+    {
+        Task<IEnumerable<User>> getuser();
+        Task<User> AuthenticateUser(string username, string passcode);
+    }
+}
